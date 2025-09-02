db148f70aa5a3f135587e1b2e822f204afda1aec
GsX07 <zreflect12@gmail.com>
2025-09-02 10:11:21 -0300

    Now the game board can spawn Mine Tiles Randomlygit status

--- Assets/Scripts/GameBoard.cs
+++ Assets/Scripts/GameBoard.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class GameBoard : MonoBehaviour
 {
     
     [SerializeField] private Tile _tileToSpawn;
     [SerializeField] private Vector2Int _boardSize;
+    //Mine position relative to the boardTile and not the Tile position.
+    [SerializeField] private List<Vector2Int> _minePositions = new List<Vector2Int>();
 
     private Tile[,] _tiles;
 
@@ -47,9 +50,11 @@ public class GameBoard : MonoBehaviour
                 tileXSize = (int)tile.transform.localScale.x;
                 tileZSize = (int)tile.transform.localScale.z;
 
+                //Here we need to take the tileSize into consideration, otherwise they'll overlap each other.
                 tile.transform.position = new Vector3(i * tileXSize, 0f, j * tileZSize);
 
-                tile.name = $"x: {tile.transform.position.x}, z: {tile.transform.position.z}";
+                //Reference in the grid only
+                tile.name = $"xTile: {i}, yTile: {j}";
 
                 //Initializing the data for each created tile
                 TileData tileData = new TileData()
@@ -64,7 +69,48 @@ public class GameBoard : MonoBehaviour
 
                 //Sets the newTileData
                 tile.InitializeData(tileData);
+                
+            }
+            
+        }
+        SetMinesRandomly();
+    }
+
+    private void SetMine(Vector2Int minePosition)
+    {
+        _tiles[minePosition.x, minePosition.y].Data.TiType = TileData.TileType.Mine;
+
+        _minePositions.Add(minePosition);
+    }
+
+    private void SetMinesRandomly()
+    {
+
+        int maxMineAmount = 10;
+
+        int randomX;
+        int randomY;
+
+        Vector2Int randomPos;
+
+        while (_minePositions.Count < maxMineAmount)
+        {
+            //Gets a random position between the 0 and the gameBoard
+            randomX = Random.Range(0, _boardSize.x);
+            randomY = Random.Range(0, _boardSize.y);
+            randomPos = new Vector2Int(randomX, randomY);
+
+            //if it already exists in our _minePositions list, we skip
+            if (_minePositions.Contains(randomPos))
+            {
+                continue;
+            }
+            //if not, we add the mine
+            else
+            {
+                SetMine(randomPos);
             }
         }
     }
 }
+    
